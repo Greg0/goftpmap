a22930c0b32388c118e4542763095c110f5d6a2d
Grzegorz Kuźnik <gerg0sz92@gmail.com>
2020-11-25 15:30:00 +0100

    Add first version of listing in output

---
+++ go.mod
@@ -0,0 +1,9 @@
+module ftpmap
+
+go 1.15
+
+require (
+	github.com/fatih/color v1.10.0
+	github.com/jlaffaye/ftp v0.0.0-20201112195030-9aae4d151126
+	golang.org/x/sys v0.0.0-20201119102817-f84b799fce68 // indirect
+)
---
+++ map.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"strings"
+
+	"github.com/fatih/color"
+	"github.com/jlaffaye/ftp"
+)
+
+type Connection struct {
+	Host     string
+	Login    string
+	Password string
+	Root     string
+}
+
+func main() {
+
+	conn := Connection{
+		Host:     "",
+		Login:    "",
+		Password: "",
+		Root:     "/",
+	}
+
+	flag.StringVar(&conn.Host, "h", "", "host[:port]")
+	flag.StringVar(&conn.Login, "u", "", "User")
+	flag.StringVar(&conn.Password, "p", "", "Password")
+	flag.StringVar(&conn.Root, "root", "/", "Root directory")
+	flag.Parse()
+
+	if len(conn.Host) == 0 || len(conn.Login) == 0 || len(conn.Password) == 0 {
+		ProgramError("Missing arguments")
+	}
+
+	client, err := ftp.Dial(conn.Host)
+	ErrorCheck(err)
+
+	err = client.Login(conn.Login, conn.Password)
+	ErrorCheck(err)
+
+	fmt.Println("Connected to server")
+
+	ReadDir(client, conn.Root)
+}
+
+func ReadDir(client *ftp.ServerConn, dir string) {
+	entries, _ := client.List(dir)
+
+	for _, entry := range entries {
+		relativePath := strings.Trim(dir+"/"+entry.Name, "/")
+		if entry.Type == ftp.EntryTypeFolder {
+			ReadDir(client, relativePath)
+		} else {
+			fmt.Println(relativePath)
+		}
+	}
+}
+
+func ErrorCheck(err error) {
+	if err != nil {
+		color.Set(color.FgHiRed)
+		panic(err)
+		color.Unset()
+	}
+}
+
+func ProgramError(message string) {
+	panic(fmt.Sprintf(message))
+}
